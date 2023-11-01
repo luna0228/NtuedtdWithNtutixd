0e4823bc6bc40a5fa8e5b58bbc2e0d65a4b272ad
luna0228 <51066795+luna0228@users.noreply.github.com>
2023-11-01 16:32:31 +0800

    update 231101
    合併蓉

--- src/components/PageBanner.jsx
+++ src/components/PageBanner.jsx
@@ -1,9 +1,24 @@
 export default function PageBanner() {
 
     return (
-        <div className="pageBanner">
-            <img src="" alt="" />
-        </div>
 
+
+        <div className="pageBanner row justify-content-between">
+
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item"><a href="#">Home</a></li>
+                <li className="breadcrumb-item"><a href="#">Library</a></li>
+                <li className="breadcrumb-item active" ><a href="#">Data</a></li>
+            </ol>
+            <div className="breadcrumb2">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><a href="#">Home</a></li>
+                    <li className="breadcrumb-item"><a href="#">Library</a></li>
+                    <li className="breadcrumb-item active" ><a href="#">Data</a></li>
+                </ol>
+            </div>
+            {/* <img src="" alt="" /> */}
+
+        </div>
     )
 }
