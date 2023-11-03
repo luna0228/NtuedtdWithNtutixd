8b800ca3852150932bb2f510443cd47a448b52c1
luna0228 <51066795+luna0228@users.noreply.github.com>
2023-11-03 13:42:34 +0800

    update 231103

--- src/components/PageBanner.jsx
+++ src/components/PageBanner.jsx
@@ -35,26 +35,30 @@ export default function PageBanner({ school }) {
 
     return (
         <div className="pageBanner ">
-            <div className="row justify-content-between">
-                <ol className="breadcrumb ">
-                    <li className="breadcrumb-item"><a href="#">Home</a></li>
-                    <li className="breadcrumb-item"><a href="#">Library</a></li>
-                    <li className="breadcrumb-item active" ><a href="#">Data</a></li>
-                   
-                </ol>
-                
-                <div className=" dropdown">
-                    <a className="btn dropdown-toggle text-wrap" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        進階搜尋
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">112期末</a></li>
-                        <li><a className="dropdown-item" href="#">112期中</a></li>
-                        <li><a className="dropdown-item" href="#">111期末</a></li>
-                        <li><a className="dropdown-item" href="#">111期中</a></li>
-                    </ul>
+            <div className="container">
+                <div className="topBar">
+                    <ol className="breadcrumb ">
+                        <li className="breadcrumb-item"><a href="#">Home</a></li>
+                        <li className="breadcrumb-item"><a href="#">Library</a></li>
+                        <li className="breadcrumb-item active" ><a href="#">Data</a></li>
+
+                    </ol>
+
+                    <div className=" dropdown">
+                        <a className="btn dropdown-toggle text-wrap" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            進階搜尋
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="#">112期末</a></li>
+                            <li><a className="dropdown-item" href="#">112期中</a></li>
+                            <li><a className="dropdown-item" href="#">111期末</a></li>
+                            <li><a className="dropdown-item" href="#">111期中</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
+
+
             <div className="container page-container">
                 <div className="bannerImg">
                     <img src={pageBannerMB()} alt={`banner-${school}`} className="hide-md" />
