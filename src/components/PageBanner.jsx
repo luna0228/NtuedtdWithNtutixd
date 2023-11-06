cd7e77f5e0718c45388c1445b59bd0a3c1d407ca
luna0228 <51066795+luna0228@users.noreply.github.com>
2023-11-06 11:39:31 +0800

    update 231106

--- src/components/PageBanner.jsx
+++ src/components/PageBanner.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import bannerNtut from "../assets/PageBannerNtut2.png"
 import bannerNtutSm from "../assets/PageBannerNtut2-sm.png"
 import bannerNtue from "../assets/PageBannerNtue2.png"
@@ -51,11 +52,12 @@ export default function PageBanner({ school, semester }) {
                             分類
                         </a>
                         <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href={`/works/${school}/112-1`}>112期中</a></li>
-                            <li><a className="dropdown-item" href={`/works/${school}/111-2`}>111期末</a></li>
-                            <li><a className="dropdown-item" href={`/works/${school}/111-1`}>111期中</a></li>
-                            <li><a className="dropdown-item" href={`/works/${school}/110-2`}>110期末</a></li>
-                            <li><a className="dropdown-item" href={`/works/${school}/110-1`}>110期中</a></li>
+                            <li>
+                                <Link to={`/works/${school}/112-1`} className="dropdown-item">112期中</Link></li>
+                            <li><Link to={`/works/${school}/111-2`} className="dropdown-item">111期末</Link></li>
+                            <li><Link to={`/works/${school}/111-1`} className="dropdown-item">111期中</Link></li>
+                            <li><Link to={`/works/${school}/110-2`} className="dropdown-item">110期末</Link></li>
+                            <li><Link to={`/works/${school}/110-1`} className="dropdown-item">110期中</Link></li>
                         </ul>
                     </div>
                 </div>
